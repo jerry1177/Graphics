21653575ede320f1baca93c8804228dc25c41bca
jerry1177 <jerrymaldonadom@gmail.com>
2020-08-22 17:22:31 -0700

    Initial Commit

---
+++ OpenGL/src/app.cpp
@@ -0,0 +1,66 @@
+#include "hzpch.h"
+#include <iostream>
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
+
+void processInput(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+}
+
+int main() {
+	std::cout << "Hello world!\n";
+	
+
+	//*
+
+	GLFWwindow* window;
+
+	//* Initialize the library 
+	if (!glfwInit())
+		return -1;
+
+	//* Create a windowed mode window and its OpenGL context 
+	window = glfwCreateWindow(1280, 960, "Hello World", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
+		return -1;
+	}
+
+	//* Make the window's context current 
+	glfwMakeContextCurrent(window);
+
+	// load glad
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
+	glViewport(0, 0, 800, 600);
+
+
+
+	//* Loop until the user closes the window 
+	while (!glfwWindowShouldClose(window))
+	{
+		processInput(window);
+		//* Render here 
+		glClearColor(0.9f, 0.9f, 0.9f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		//* Swap front and back buffers 
+		glfwSwapBuffers(window);
+
+		//* Poll for and process events 
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	
+	system("pause");
+	//*/
+	//return 0;
+}
---
+++ premake5.lua
@@ -0,0 +1,95 @@
+workspace "Graphics"
+	architecture "x64"
+	startproject "OpenGL"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+IncludeDir = {}
+IncludeDir["GLFW"] = "OpenGL/vendor/GLFW/include"
+IncludeDir["Glad"] = "OpenGL/vendor/Glad/include"
+IncludeDir["ImGui"] = "OpenGL/vendor/imgui"
+IncludeDir["glm"] = "OpenGL/vendor/glm"
+IncludeDir["stb_image"] = "OpenGL/vendor/stb_image"
+
+include "OpenGL/vendor/GLFW"
+include "OpenGL/vendor/GLAD"
+
+project "OpenGL"
+	location "OpenGL"
+	kind "ConsoleApp"
+	language "C++"
+	cppdialect "C++17"
+	staticruntime "on"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	pchheader "hzpch.h"
+	pchsource "OpenGL/src/hzpch.cpp"
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp",
+		"%{prj.name}/vendor/stb_image/**.cpp",
+		"%{prj.name}/vendor/glm/glm/**.hpp",
+		"%{prj.name}/vendor/glm/glm/**.inl"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/src",
+		"%{prj.name}/vendor/spdlog/include",
+		"%{IncludeDir.GLFW}",
+		"%{IncludeDir.Glad}",
+		"%{IncludeDir.ImGui}",
+		"%{IncludeDir.glm}",
+		"%{IncludeDir.stb_image}"
+	}
+
+	links {
+		"GLFW",
+		"GLAD"
+	}
+	
+
+
+	filter "system:windows"
+		
+		systemversion "latest"
+
+		defines
+		{
+			"_GLFW_WIN32",
+			"_WIND32",
+			"_WIND64",
+			"HZ_BUILD_DLL",
+			"GLFW_INCLUDE_NONE",
+			"_CRT_SECURE_NO_WARNINGS"
+		}
+		
+
+	filter "configurations:Debug"
+		defines "HZ_DEBUG"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		defines "HZ_RELEASE"
+		runtime "Release"
+		optimize "on"
+		symbols "on"
+
+	filter "configurations:Dist"
+		defines "HZ_DIST"
+		runtime "Release"
+		optimize "on"
+		symbols "on"
+
